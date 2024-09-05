ac012ea2e05125f0c3266a0eeec554d69ecf9db4
Gopikrishna1033 <venkatagopikrishna007@gmail.com>
2024-09-05 12:50:31 +0530

    task

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+import Gopi from "../assets/Gopi.jpg"
 function Home() {
   return (
     <div className='body'>
@@ -8,17 +9,23 @@ function Home() {
           <div className="col-md-12 lcm">
             <h1>Welcome to my portfolio</h1>
           </div>
+          <img src={Gopi} alt="description"  loading='lazy' className='gopi' width={300}/>
           <p>This is Venkatagopikrishna a <u>Software Engineer</u></p>
+
+          
+          <button className='know-btn btn btn-outline-primary '>Know about me</button>
+          <button className='contact-btn btn btn-outline-primary'>Contact me</button>
         </div>
-        <div className="row">
+        
+        {/* <div className="row">
           <div className="col-md-12">
-          <button className='know-btn btn btn-outline-primary'>Know about me</button>
+          <button className='know-btn btn btn-outline-primary '>Know about me</button>
           <button className='contact-btn btn btn-outline-primary'>Contact me</button>
           </div>
-        </div>
+        </div> */}
 
       </div>
-     
+      
     </div>
   )
 }
